606163ca38a0ccec89177cee2774ac852caa16ee
Gabriel Cunha <gsalomaoc@gmail.com>
2021-09-24 14:39:36 -0300

    Refactoring for testing

--- app/server.js
+++ app/server.js
@@ -1,33 +1,9 @@
 'use strict';
 
-const express = require('express');
-const router = require('./routes')
+const app = require('./app.js');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
-
-//Conecta e sincroniza o banco de dados com os models atuais
-(async () => {
-    const database = require('./db');
-    const User = require('./models');
- 
-    try {
-        const resultado = await database.sync();
-        console.log('Connected to DB succesfuly');
-    } catch (error) {
-        console.log(error);
-    }
-})();
-
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => res.send('App is working'))
-
-app.use('/', router)
 
 
 app.listen(PORT, HOST);
